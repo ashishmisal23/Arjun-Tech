bf728ab7f968e18fc529ff7369baa2f99e25af94
ashishmisal23 <ashishmisal23@gmail.com>
2025-11-25 02:43:29 +0530

    updated axios file

--- src/Utils/axios.js
+++ src/Utils/axios.js
@@ -1,30 +1,20 @@
 import axios from "axios";
-import { getEnvironmentUrl } from "./utils";
+import { getEnvironmentUrl } from './utils.js'
 
 export const submitContactForm = async (formData) => {
     const url = getEnvironmentUrl();
 
     const payload = {
-        name: formData?.name,
-        mobile: formData?.mobile,
-        email: formData?.email,
-        message: formData?.message,
-    };
-    const headers = {
-        "Content-Type": "application/json",
+        name: formData.name,
+        mobile: formData.mobile,
+        email: formData.email,
+        message: formData.message,
     };
 
     try {
-        const response = await axios.post(
-            url,
-            payload,
-            { headers }
-        );
-        if (response?.data?.success === 'true' || response?.success === 'true') {
-            return true;
-        }
-        return false;
-    } catch (error) {
-        throw error;
+        const res = await axios.post(url, payload);
+        return res.status === 200;
+    } catch (err) {
+        throw err;
     }
 };
